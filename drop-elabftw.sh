50800fdce78f50b2053aef2b1d5c0f644c64e0e0
NicolasCARPi <nicolas.carpi@curie.fr>
2016-04-27 22:23:13 +0200

    switch to docker

--- drop-elabftw.sh
+++ drop-elabftw.sh
@@ -3,8 +3,6 @@
 logfile='elabftw.log'
 rootpass=$(tr -dc A-Za-z0-9 < /dev/urandom | head -c 12 | xargs)
 pass=$(tr -dc A-Za-z0-9 < /dev/urandom | head -c 12 | xargs)
-echo "Password for MySQL user 'elabftw' : $pass" >> $logfile
-echo "Password for MySQL user 'root': $rootpass" >> $logfile
 
 # display ascii logo
 clear
@@ -18,90 +16,50 @@ echo ""
 # get info for letsencrypt and nginx
 echo "[:)] Welcome to the install of elabftw!"
 echo ""
-echo "[?] What is the domain name of this server?"
-echo "[!] WARNING: don't put the IP address!"
-echo "[*] Example : elabftw.ktu.edu"
-read -p "[?] Your domain name: " domain
-echo "[?] Second and last question, what is your email?"
-echo "[!] It is sent only to letsencrypt"
-read -p "[?] Your email: " email
 echo "[*] You can follow the status of the install with"
-echo "[*] Do Ctrl-b, release and press '%'"
+echo "[*] Ctrl-b, release and press '%'"
 echo "[$] tail -f $logfile"
 echo ""
 
-echo "[*] Installing nginx, php, mysql, openssl and git"
-
+echo "[*] Updating packages list"
 apt-get update >> $logfile 2>&1
-#apt-get upgrade
-DEBIAN_FRONTEND=noninteractive apt-get -y install \
-    nginx \
-    mysql-server \
-    openssl \
-    php5-fpm \
-    php5-mysql\
-    php-apc \
-    php5-gd \
-    php5-curl \
-    python-setuptools \
-    curl \
-    git \
-    software-properties-common >> $logfile 2>&1
 
-echo "[*] Configuring server"
-# nginx config
-sed -i -e"s/keepalive_timeout\s*65/keepalive_timeout 2/" /etc/nginx/nginx.conf
-sed -i -e"s/keepalive_timeout 2/keepalive_timeout 2;\n\tclient_max_body_size 100m/" /etc/nginx/nginx.conf
+echo "[*] Installing python-pip"
+apt-get install python-pip >> $logfile 2>&1
 
-# php-fpm config
-sed -i -e "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" /etc/php5/fpm/php.ini
-sed -i -e "s/upload_max_filesize\s*=\s*2M/upload_max_filesize = 100M/g" /etc/php5/fpm/php.ini
-sed -i -e "s/post_max_size\s*=\s*8M/post_max_size = 100M/g" /etc/php5/fpm/php.ini
+echo "[*] Installing docker-compose"
+pip install -U docker-compose >> $logfile 2>&1
 
-# nginx site conf
-service nginx stop
-wget -qO /etc/nginx/sites-available/default https://raw.githubusercontent.com/elabftw/drop-elabftw/master/nginx-site.conf
-sed -i "s/DOMAIN/$domain/g" /etc/nginx/sites-available/default
+echo "[*] Creating folder structure"
+mkdir -pvm 777 /elabftw/{web,mysql} >> $logfile 2>&1
 
-echo "[*] Generating Diffie-Hellman group (can be a bit long)"
-openssl dhparam -out /etc/nginx/dhparams.pem 2048 2>&1
+echo "[*] Grabbing the docker-compose configuration file"
+wget -q https://raw.githubusercontent.com/elabftw/docker-elabftw/master/src/docker-compose.yml-EXAMPLE -O docker-compose.yml
 
-echo "[*] Installing letsencrypt in /letsencrypt"
-git clone --depth 1 -b master https://github.com/letsencrypt/letsencrypt /letsencrypt >> $logfile 2>&1
+echo "[*] Adjusting configuration"
+# elab config
+secret_key=$(curl -s https://demo.elabftw.net/install/generateSecretKey.php)
+sed -i -e "s/SECRET_KEY=/SECRET_KEY=$secret_key/" docker-compose.yml
+sed -i -e "s/SERVER_NAME=localhost/SERVER_NAME=$domain/" docker-compose.yml
+sed -i -e "s:/dok/uploads:/elabftw/web:" docker-compose.yml
 
-echo "[*] Getting the SSL certificate"
-cd /letsencrypt && ./letsencrypt-auto certonly --email $email --agree-tos -d $domain
+# mysql config
+sed -i -e "s/MYSQL_ROOT_PASSWORD=secr3t/MYSQL_ROOT_PASSWORD=$rootpass/" docker-compose.yml
+sed -i -e "s/MYSQL_PASSWORD=secr3t/MYSQL_PASSWORD=$pass/" docker-compose.yml
+sed -i -e "s/DB_PASSWORD=secr3t/DB_PASSWORD=$pass/" docker-compose.yml
+sed -i -e "s:/dok/mysql:/elabftw/mysql:" docker-compose.yml
 
-echo "[*] Installing elabftw in /elabftw"
-# elabftw
-git clone --depth 1 -b master https://github.com/elabftw/elabftw.git /elabftw >> $logfile 2>&1
-# fix permissions
-chown -R www-data:www-data /elabftw
+echo "[*] Launching docker"
+docker-compose up -d
 
-# create the elabftw database
-echo "[*] Giving a password to MySQL root account"
-echo "UPDATE mysql.user SET Password=PASSWORD('$rootpass') WHERE User='root';
-FLUSH PRIVILEGES;" | mysql -u root
-# now we put the password in the config file
-sed -i "/\[client\]/a user = root \\npassword = $rootpass" /etc/mysql/my.cnf
+#echo "[*] Installing letsencrypt in /letsencrypt"
+#git clone --depth 1 -b master https://github.com/letsencrypt/letsencrypt /letsencrypt >> $logfile 2>&1
 
-echo "[*] Creating the  elabftw database and user with random password"
-echo "create database elabftw;" | mysql -u root -p$rootpass
-echo "grant usage on *.* to elabftw@localhost identified by '$pass';" | mysql -u root -p$rootpass
-echo "grant all privileges on elabftw.* to elabftw@localhost;" | mysql -u root -p$rootpass
-
-echo "[*] Starting php-fpm"
-service php5-fpm restart
-echo "[*] Starting nginx"
-service nginx start
+#echo "[*] Getting the SSL certificate"
+#cd /letsencrypt && ./letsencrypt-auto certonly --email $email --agree-tos -d $domain
 
 ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
 echo "Congratulations, eLabFTW is now running! :)\n
-====> Go to https://$ip/install now ! <====\n
-Host for mysql database : localhost\n
-Name of the database : elabftw\n
-Username to connect to MySQL server : elabftw\n
-Password : $pass\n
+echo "It might take a minute to run at first.\n"
 ====> Go to https://$ip/install now ! <====\n
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
-echo "The password is also stored in the beginning of the log file: $logfile"
