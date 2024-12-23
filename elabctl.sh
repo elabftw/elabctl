5a37c997c2c77daef83627682a3ec553e22651d2
Nicolas CARPi <nico-git@deltablot.email>
2024-12-23 13:39:24 +0100

    5.0.4

--- elabctl.sh
+++ elabctl.sh
@@ -3,7 +3,7 @@
 # https://github.com/elabftw/elabctl/
 # © 2022 Nicolas CARPi @ Deltablot
 # License: GPLv3
-declare -r ELABCTL_VERSION='5.0.3'
+declare -r ELABCTL_VERSION='5.0.4'
 
 # default backup dir
 declare BACKUP_DIR='/var/backups/elabftw'
